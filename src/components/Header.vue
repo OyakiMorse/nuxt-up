ab2935effc458aa485de303aa1581ab68f59043f
Pavlo <koval.pavlo@itspace.company>
2022-12-16 12:17:52 +0200

    add form && header

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header__content">
+        <NuxtLink to="/" class="header__logo"> NuxtUP </NuxtLink>
+
+        <ul class="header__list">
+          <li v-for="item in items" :key="item" class="header__list-item">
+            <NuxtLink class="header__list-link" to="#">
+              {{ item }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup>
+  const items = ['Home', 'Garden', 'Pool', 'Valley'];
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,104 @@
 <template>
-  <div>
-    <h1 class="header">index</h1>
+  <div class="inital">
+    <div class="inital__bg">
+      <img src="../public/img/bg3.png" alt="bg" class="inital__bg-img" />
+    </div>
+
+    <Header />
+
+    <div class="container">
+      <main class="inital__content">
+        <div class="inital__card">
+          <h1 class="inital__card-title">Welcome to <span>NuxtUP</span></h1>
+          <p class="inital__card-subtitle">Register for try my code</p>
+
+          <form action="" class="inital__form">
+            <div class="inital__form-inputs">
+              <div class="inital__form-field">
+                <label for="email" class="inital__form-label">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  class="inital__form-input"
+                  placeholder="Enter your email"
+                  required
+                  id="userEmail"
+                />
+                <span class="inital__form-error">Error</span>
+              </div>
+
+              <div class="inital__form-field">
+                <label for="password" class="inital__form-label">
+                  Password
+                </label>
+
+                <div class="inital__form-box">
+                  <input
+                    type="password"
+                    name="password"
+                    class="inital__form-input"
+                    placeholder="Enter your password"
+                    required
+                    id="userPswd"
+                    ref="inputPswd"
+                  />
+                  <img
+                    @click="showHiddenPswd(inputPswd, inputIconEye)"
+                    src="../public/img/eye.svg"
+                    alt="eye"
+                    class="icon-eye"
+                    id="input-icon"
+                    ref="inputIconEye"
+                  />
+                </div>
+
+                <span class="inital__form-error">Error</span>
+              </div>
+
+              <div class="inital__form-buttons">
+                <button @submit.prevent="submitForm" class="inital__form-btn">
+                  Register
+                </button>
+                <button
+                  @click="notSupportMsg"
+                  class="inital__form-btn inital__form-btn--light"
+                >
+                  Login
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </main>
+    </div>
   </div>
 </template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  const inputIconEye = ref(null);
+  const inputPswd = ref(null);
+
+  const form = {
+    email: '',
+    password: '',
+    agreeConfirm: [],
+  };
+
+  const showHiddenPswd = (inputPswd, inputIcon) => {
+    if (inputPswd.type === 'password') {
+      if (inputPswd.value !== '') {
+        inputPswd.type = 'text';
+        inputIcon.src = '_nuxt/public/img/eye-fill.svg';
+      }
+    } else {
+      inputPswd.type = 'password';
+      inputIcon.src = '_nuxt/public/img/eye.svg';
+    }
+  };
+
+  function notSupportMsg() {
+    alert('This feature unavailable now');
+  }
+</script>
